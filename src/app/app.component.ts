90eff929dae77ea8228b6680e7909535f655db25
Mayur Shah <mshah-ror@codal.com>
2018-07-16 18:52:25 +0530

    WIP - Impelemented Socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  greetings: string[] = [];
+  showConversation: boolean = false;
+  ws: any;
+  name: string;
+  disabled: boolean;
+
+  constructor(){}
+
+  connect() {
+  	var headers = new Headers();
+    headers.append('Access-Control-Allow-Origin' , '*');
+    //connect to stomp where stomp endpoint is exposed
+    let socket = new SockJS("http://206.189.136.143:8010");
+    // let socket = new WebSocket("http://206.189.136.143:8010");
+    this.ws = Stomp.over(socket);
+    let that = this;
+    this.ws.connect({}, function(frame) {
+      that.ws.subscribe("/errors", function(message) {
+        alert("Error " + message.body);
+      });
+      that.ws.subscribe("/orders", function(message) {
+        console.log(message)
+        that.showGreeting(message.body);
+      });
+      that.disabled = true;
+    }, function(error) {
+      alert("STOMP error " + error);
+    });
+  }
+
+  disconnect() {
+    if (this.ws != null) {
+      this.ws.ws.close();
+    }
+    this.setConnected(false);
+    console.log("Disconnected");
+  }
+
+  sendName() {
+    let data = JSON.stringify({
+      'name' : this.name
+    })
+    this.ws.send("/app/message", {}, data);
+  }
+
+  showGreeting(message) {
+    this.showConversation = true;
+    this.greetings.push(message)
+  }
+
+  setConnected(connected) {
+    this.disabled = connected;
+    this.showConversation = connected;
+    this.greetings = [];
+  }
 }
